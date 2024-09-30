b529e0187d3ca595b0b50331f662511dd118c4e7
cdeng65 <cdeng65@gatech.edu>
2024-09-29 21:14:20 -0400

    seed locations with corrected schema

--- src/app/LocationPage/page.tsx
+++ src/app/LocationPage/page.tsx
@@ -1,28 +1,64 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
+import mongoose from 'mongoose';
 
-const locations = [
-  {
-    name: 'Location Name',
-    contact: '123-456-7890',
-    address: '1234 Address Name, Apt. 56, Atlanta, GA',
-    type: 'Drop-Off',
-    bags: 1234,
-    notes: 'Additional Notes are placed here, and will include info about that in case it’s necessary, so this is where that...',
-  },
-  {
-    name: 'Location Name',
-    contact: '123-456-7890',
-    address: '1234 Address Name, Apt. 56, Atlanta, GA',
-    type: 'Pick-Up',
-    bags: 1234,
-    notes: 'Additional Notes are placed here, and will include info about that in case it’s necessary, so this is where that...',
-  },
-];
 
-const Dashboard = () => {
+import { getAllLocations } from "@/server/db/actions/location";
+import { Location } from "@/server/db/models/location";
+
+
+// const locations = [
+//   {
+//     name: 'Location Name',
+//     contact: '123-456-7890',
+//     address: '1234 Address Name, Apt. 56, Atlanta, GA',
+//     type: 'Drop-Off',
+//     bags: 1234,
+//     notes: 'Additional Notes are placed here, and will include info about that in case it’s necessary, so this is where that...',
+//   },
+//   {
+//     name: 'Location Name',
+//     contact: '123-456-7890',
+//     address: '1234 Address Name, Apt. 56, Atlanta, GA',
+//     type: 'Pick-Up',
+//     bags: 1234,
+//     notes: 'Additional Notes are placed here, and will include info about that in case it’s necessary, so this is where that...',
+//   },
+// ];
+
+function LocationDashboardPage() {
+  const [locations, setLocations] = useState<Location[]>([]);
+  const [sortOption, setSortOption] = useState<string>('alphabetically');
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      const response = await getAllLocations();
+      const data = JSON.parse(response || "[]");
+
+      if (sortOption === 'alphabetically') {
+        data.sort((a: Location, b: Location) => a.locationName.localeCompare(b.locationName));
+      } else if (sortOption === 'byType') {
+        data.sort((a: Location, b: Location) => {
+          if (a.type === b.type) {
+            return 0;
+          }
+          return a.type === 'Drop-Off' ? -1 : 1;
+        });
+      }
+
+      setLocations(data || []);
+    };
+    fetchLocations();
+  }, [sortOption]);
+
+
+  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortOption(event.target.value);
+  };
+
+  
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -36,7 +72,8 @@ const Dashboard = () => {
           className={styles.searchInput}
         />
         <div className={styles.filterControls}>
-          <select className={styles.sortSelect}>
+          <h1>Sorted:</h1>
+          <select className={styles.sortSelect} value={sortOption} onChange={handleSortChange}>
             <option value="alphabetically">Alphabetically</option>
             <option value="byType">By Type</option>
           </select>
@@ -57,10 +94,10 @@ const Dashboard = () => {
             <div key={index} className={styles.locationCard}>
               <div className={styles.locationDetails}>
                 <div className={styles.locationInfo}>
-                  <strong>{location.name}</strong>
+                  <strong>{location.locationName}</strong>
                   <div>{location.contact}</div>
                 </div>
-                <div className={styles.locationInfo}>{location.address}</div>
+                <div className={styles.locationInfo}>{location.address.street + ", " + location.address.city + ", " + location.address.state + " " + location.address.zipCode}</div>
                 <div className={`${styles.locationInfo} ${location.type === 'Pick-Up' ? styles.pickUp : styles.dropOff}`}>
                   {location.type}
                 </div>
@@ -76,4 +113,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default LocationDashboardPage;
