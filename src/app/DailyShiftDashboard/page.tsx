4a5d87cb6f6a9cf8669fd4abe79065ac4e539774
Peter <paimasiko3@gatech.edu>
2024-10-19 16:38:27 -0400

    Create page.tsx

---
+++ src/app/DailyShiftDashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+
+function DailyShiftDashboardPage() {
+    return (
+        <div>
+        <h1>Daily Shift Dashboard</h1>
+        </div>
+    );
+}
+
+export default DailyShiftDashboardPage;
