dcc41cca98a6e6dd37e8fa6c6f5ed8481fd8a672
Joanna Cheng <joannacheng21@outlook.com>
2024-11-09 16:02:18 -0500

    fix page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 import Image from "next/image";
 import LocationDashboardPage from "./LocationPage/page";
-import Analytics from "./Analytics";
 
 export default function Home() {
-  return (
-    // <Analytics></Analytics>
-    <div style={{ display: "flex" }}>
-      <div style={{ width: "20%" }}></div>
-      <Analytics />
-    </div>
-  );
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <LocationDashboardPage />
