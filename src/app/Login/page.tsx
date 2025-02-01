2a3332d3d8b7a30af8fec6ec5290a6dd489ed86b
cindyz0 <cindy.zhu1@gmail.com>
2025-01-31 22:27:30 -0500

    implemented proper designs for login page

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -20,50 +20,65 @@ export default function LoginScreen() {
   const [errorBannerMsg, setErrorBannerMsg] = useState("");
 
   return (
-    <div className="flex absolute bg-white">
+    <div className="flex absolute bg-[#D6E9FF]">
       <div className="h-screen w-screen">
         <div className="flex flex-col w-full h-full sm:flex-row">
           <HalfScreen />
-          <div className="flex flex-col w-full h-full justify-center items-center mt-8 sm:mt-0 sm:w-1/2">
+          <div className="flex flex-col w-full h-full justify-center items-center mt-8 sm:mt-0 sm:w-1/2 bg-white rounded-3xl">
             <div className={`flex flex-col w-[90%] sm:w-[60%] sm:items-center`}>
               {/* {errorBannerMsg && (
                 <div className="hidden sm:inline">
                   <Banner text={errorBannerMsg} />
                 </div>
               )} */}
-              <p
-                className={`text-primary-text text-2xl font-bold font-opensans mb-10 sm:order-1`}
-              >
-                Log In
-              </p>
+              <div className="flex justify-start w-[100%]">
+                <p className="text-left text-primary-text text-2xl font-bold font-opensans text-[#013779] text-4xl mb-2">
+                  Sign in
+                </p>
+              </div>
               {/* {errorBannerMsg && (
                 <div className="inline -mt-8 sm:hidden sm:mt-0">
                   <Banner text={errorBannerMsg} />
                 </div>
               )} */}
+              <div className="flex justify-start w-[100%] mb-7">
+                <p className="text-gray-400">
+                  Enter your email and password to sign in!
+                </p>
+              </div>
               <div className="flex flex-col w-full sm:order-2">
-                <div className="sm:mb-6">
-                  <TextInput
-                    label="Email"
-                    formValue={register("email", {
-                      validate: (v) =>
-                        !v ? "Email cannot be empty." : undefined,
-                    })}
-                    error={formState.errors.email?.message}
-                  />
-                </div>
+                <TextInput
+                  label="Email"
+                  placeholder="example@email.com"
+                  formValue={register("email", {
+                    validate: (v) =>
+                      !v ? "Email cannot be empty." : undefined,
+                  })}
+                  error={formState.errors.email?.message}
+                />
                 <TextInput
                   label="Password"
                   inputType="password"
+                  placeholder="Minimum 8 Characters"
                   formValue={register("password", {
                     validate: (v) =>
                       !v ? "Password cannot be empty." : undefined,
                   })}
                   error={formState.errors.password?.message}
                 />
-                <div className="flex justify-end mb-9 sm:mb-10">
+                <div className="flex justify-center mb-4">
+                  <hr className="w-[45%] my-3 mr-5 border-t-2"/>
+                    <span className="text-gray-400">or</span>
+                  <hr className="w-[45%] my-3 ml-5 border-t-2"/>
+                </div>
+                <Button type="Google" text="Sign in with Google"></Button>
+                <div className="flex justify-between mb-7 sm:mb-7">
+                  <div className="flex justify-start">
+                    <input className='rounded-none checked:bg-[#016ff3] mr-2' type="checkbox"/>
+                    <p className="text-[#016ff3] font-opensans text-sm">Keep me logged in</p>
+                  </div>
                   <button
-                    className="w-auto text-center text-mbb-pink text-sm font-semibold font-opensans"
+                    className="w-auto text-center text-mbb-pink text-sm font-semibold font-opensans text-[#016ff3]" 
                     onClick={() => {
                       const email = getValues().email;
 
@@ -74,12 +89,12 @@ export default function LoginScreen() {
                       );
                     }}
                   >
-                    Forgot Password
+                    Forgot password?
                   </button>
                 </div>
-                <div className="mb-5 sm:mb-7">
+                <div className="mb-3 sm:mb-1">
                   <Button
-                    text="Log In"
+                    text="Continue"
                     onClick={async () => {
                       setErrorBannerMsg("");
                       const isValid = await trigger(undefined, {
@@ -106,16 +121,16 @@ export default function LoginScreen() {
                     }}
                   />
                 </div>
-                <div className="mb-10 flex justify-center items-center">
+                <div className="mb-1 flex justify-center items-center">
                 </div>
-                <div className="flex flex-row justify-center mb-8 sm:mb-0">
-                  <div className="text-light-black text-base font-normal font-opensans leading-tight tracking-tight mr-2">
+                <div className="flex flex-row justify-center mb-8 sm:mb-1">
+                  <div className="text-[#063c7c] text-base font-normal font-opensans leading-tight tracking-tight mr-2">
                     Don&apos;t have an account?&nbsp;
                     <button
-                      className="text-mbb-pink text-base font-semibold underline"
+                      className="text-mbb-pink text-base font-semibold text-[#016ff3] hover:underline"
                       onClick={() => router.push("/signup")}
                     >
-                      Sign up now
+                      Sign Up Now
                     </button>
                   </div>
                 </div>
