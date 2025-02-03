e6af2e97f0002511f561b9a6809c88b830ec2458
Peter <paimasiko3@gatech.edu>
2025-02-02 23:13:13 -0500

    Update SignUpToken.ts

--- src/server/db/models/SignUpToken.ts
+++ src/server/db/models/SignUpToken.ts
@@ -4,14 +4,12 @@ export interface ISignUpToken extends Document {
   email: string;
   token: string;
   expiration: Date;
-  used: boolean;
 }
 
 const SignUpTokenSchema: Schema = new Schema({
   email: { type: String, required: true },
   token: { type: String, required: true, unique: true },
   expiration: { type: Date, required: true },
-  used: { type: Boolean, default: false },
 });
 
 export default mongoose.models?.SignUpToken ||
