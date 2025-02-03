460663cfffdd93fb99852f5c38c4cec11d2cd749
Peter <paimasiko3@gatech.edu>
2025-02-02 23:12:57 -0500

    Update email.ts

--- src/server/db/actions/email.ts
+++ src/server/db/actions/email.ts
@@ -26,12 +26,24 @@ export async function sendVolunteerSignupEmails(emails: string[]) {
             });
     
             const signUpLink = `${process.env.BASE_URL}/signup?token=${token}`;
-    
+            const bagelsimg = "https://static.wixstatic.com/media/c95ffe_7302ff5526b34e8c81cd07b4b6eef796~mv2.png/v1/fill/w_230,h_236,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Bagel%20Rescue%20no%20background.png"
             const info = await transporter.sendMail({
-                from: 'onboarding@resend.dev',
+                from: process.env.BAGELS_EMAIL,
                 to: email,
                 subject: 'Bagel Rescue Volunteer Sign Up Invite',
-                html: `<div><span>Hello from Bagel Rescue! You have been invited to be a volunteer!</span> <span>${signUpLink}</span></div>`,
+                html: `
+                    <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+                        <img src="${bagelsimg}" alt="Bagel Rescue" style="width: 150px; height: auto;"/>
+                        <h2>Hello from Bagel Rescue! </h2>
+                        <p>You have been invited to be a volunteer. We are excited to have you be apart of our team</p>
+                        <p>We have provided a sign up link below:</p>
+                        <br/>
+                        <a href="${signUpLink}" style="color: #1a73e8;">${signUpLink}</a>
+                        <p>If you are having issues, copy and paste the link into your broswer.</p>
+                        <hr/>
+                        <p style="line-height: 14px; font-size: 12px;">This is email was sent to <a href ="${"mailto:" + email}">${email}</a> by Bits Of Good.</p>
+                    </div>
+                `,
             });
         }
 
@@ -60,4 +72,15 @@ export async function validateSignUpToken(token: string) {
         console.error("Error validating token:", error);
         throw new Error("Failed to validate token");
     }  
+}
+
+export async function deleteSignUpToken(token: string) {
+    try {
+        await dbConnect();
+        await SignUpToken.deleteOne({ token });
+        return true;
+    } catch (error) {
+        console.error("Error deleting token:", error);
+        throw new Error("Failed to delete token");
+    }
 }
