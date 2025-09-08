b90a3f3821459c063ac6e216f809bccc20b1d123
Johannes Qian <johannesq23@gmail.com>
2025-09-08 15:46:24 -0400

    add bagel image to sign up

--- src/app/signup/HalfScreen.tsx
+++ src/app/signup/HalfScreen.tsx
@@ -1,4 +1,6 @@
 import BackButton from "./BackButton";
+import Image from 'next/image';
+import bagelsPic from './bagels.png';
 
 interface Props {
   caregiver?: boolean;
@@ -14,7 +16,7 @@ export default function HalfScreen({
   return (
     <div
       style={{ background: '#D6E9FF' }}
-      className={`${hiddenOnMobile ? "hidden sm:flex" : "flex"} flex-col w-full h-[20%] justify-center items-center sm:w-1/2 sm:h-full`}
+      className={`${hiddenOnMobile ? "hidden sm:flex" : "flex"} flex-col w-full h-[20%] justify-center items-center sm:w-[70%] sm:h-full`}
     >
       {backButtonFunction && (
         <div className="flex sm:hidden flex-start w-[90%] mt-2 -mb-5">
@@ -22,9 +24,7 @@ export default function HalfScreen({
         </div>
       )}
       <div className="flex flex-col w-full justify-center items-center shrink-0 text-black text-center font-opensans tracking-wide">
-        <p className="hidden sm:flex font-bold uppercase text-3xl sm:text-4xl">
-          Bagel <br /> Rescue
-        </p>
+        <Image src={bagelsPic} alt="Bagels Logo" />
       </div>
     </div>
   );
