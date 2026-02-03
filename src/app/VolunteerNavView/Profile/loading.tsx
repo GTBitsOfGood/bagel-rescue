4f95f3fee01199e26df13b13d076c4a9e16efb7b
jpise3 <63403110+jaipise@users.noreply.github.com>
2026-02-03 13:53:22 -0500

    fix duplication

--- src/app/VolunteerNavView/Profile/loading.tsx
+++ src/app/VolunteerNavView/Profile/loading.tsx
@@ -1,10 +1,1 @@
-import React from "react";
-import LoadingFallback from "@/app/components/LoadingFallback";
-
-export default function Loading() {
-  return (
-    <div className="min-h-[60vh] flex items-center justify-center">
-      <LoadingFallback />
-    </div>
-  );
-}
+export { default } from "@/app/components/RouteLoading";
